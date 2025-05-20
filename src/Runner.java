6641357d37d2b91b570955aa34ab33d139efc52e
1965862 <1965862@01-18116.pusd.dom>
2025-05-20 11:03:49 -0700

    init commit

---
+++ src/Runner.java
@@ -0,0 +1,11 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+		System.out.println("testing");
+	}
+
+}
