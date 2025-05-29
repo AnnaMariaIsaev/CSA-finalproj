e596f5ae953cdd819528e00112b8e7b72b26960d
avazzzzzzz <142530934+avazzzzzzz@users.noreply.github.com>
2025-05-29 11:34:33 -0700

    Add files via upload

---
+++ src/LeftLeg.java
@@ -0,0 +1,52 @@
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+public class LeftLeg {
+	private Image forward;
+	private AffineTransform tx;
+	int dir = 0; // 0-forward, 1-backward, 2-left, 3-right
+	int width, height;
+	int x, y; // position of the object
+	int vx, vy; // movement variables
+	double scaleWidth = 6.2; // change to scale image
+	double scaleHeight = 5.73; // change to scale image
+	public LeftLeg() {
+		forward = getImage("leftleg1.png"); // load the image for Tree
+		// alter these
+		width = 600;
+		height = 800;
+		x = 0;
+		y = 0;
+		tx = AffineTransform.getTranslateInstance(0, 0);
+		init(x, y); // initialize the location of the image
+					// use your variables
+	}
+	public LeftLeg(int x, int y) {
+		this();
+		this.x = x;
+		this.y = y;
+	}
+	public void paint(Graphics g) {
+		// these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		init(x, y);
+		g2.drawImage(forward, tx, null);
+	}
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(scaleWidth, scaleHeight);
+	}
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = LeftLeg.class.getResource("/" + path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+}
